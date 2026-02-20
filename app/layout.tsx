c21e7ab8bde97ae7c5e29002308f97379b581a8a
Kevin Gross <kev.gross97@gmail.com>
2026-02-20 12:54:23 +0100

    font styling to normal

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,7 @@
 import { RootProvider } from 'fumadocs-ui/provider/next';
 import type { ReactNode } from 'react';
 import './globals.css';
-import { Inter } from 'next/font/google';
 
-const inter = Inter({ subsets: ['latin'] });
 
 export default function RootLayout({
   children,
@@ -11,7 +9,7 @@ export default function RootLayout({
   children: ReactNode;
 }) {
   return (
-    <html lang="en" className={inter.className} suppressHydrationWarning>
+    <html lang="en" suppressHydrationWarning>
       <body className="flex min-h-screen flex-col">
         <RootProvider>{children}</RootProvider>
       </body>
